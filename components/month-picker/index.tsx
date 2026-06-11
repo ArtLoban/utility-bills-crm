e775d331ce61d3cf76c43af2b9aaf9f059714ce3
Art Dev <tymanrock@gmail.com>
2026-06-11 22:51:09 +0300

    fix(month-picker): keep popover open inside modal dialog

--- components/month-picker/index.tsx
+++ components/month-picker/index.tsx
@@ -48,7 +48,7 @@ export const MonthPicker = React.forwardRef<HTMLButtonElement, TProps>(
     };
 
     return (
-      <Popover open={open} onOpenChange={setOpen}>
+      <Popover open={open} onOpenChange={setOpen} modal>
         <PopoverTrigger asChild>
           <button
             ref={ref}
